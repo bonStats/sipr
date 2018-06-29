3ce141b05fca8ec7f77448817ee779cad6138670
Joshua Bon <joshuajbon@gmail.com>
2018-06-29 12:59:31 +1000

    Added second problem to problem set.

--- R/sip-problem-set.R
+++ R/sip-problem-set.R
@@ -2,6 +2,9 @@
 #'
 #' Create SIP objects \code{sipo} from classis SIP problems.
 #'
+#' Problem 1: Original problem
+#' Problem 2: # Ex. 3 Sect. 4 JJ Ruckman & A Shapiro (2009)
+#'
 #' @param number Problem number
 #' @return A predefined SIP problem
 #' @export
@@ -28,9 +31,25 @@ sip_problem <- function(number){
                 )
 
 
+  } else if(number == 2){
+    # Ex. 3 Sect. 4 JJ Ruckman & A Shapiro (2009)
+    prob <- sip(obj.fn = function(x){ -x[1]^2 + x[2] },
+                obj.gr = function(x){ c(-2 * x[1], 1) },
+                x.bounds = NULL,
+                eq.fn = NULL,
+                eq.gr = NULL,
+                eq.val = NULL,
+                ineq.fn = function(x,t){ 2 * t * x[1] - x[2] - t^2},
+                ineq.bounds = c(-Inf, 0),
+                ineq.tset = c(-1,1),
+                ineq.gr.x = function(x,t){ c(2 * t, -1)},
+                ineq.gr.t = function(x,t){  2 * x[1] - 2 * t},
+                x.feasible = c(0,0)
+    )
+
   } else {
 
-    warning("SIP problem number ", number, " not found.")
+    stop("SIP problem number ", number, " not found.")
 
   }
 
