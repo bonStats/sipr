1f3fb3a841e7fde9689d3d278b44167a3dc81d9e
Joshua Bon <joshuajbon@gmail.com>
2018-07-02 14:10:55 +1000

    Updated to be more modular.

--- R/sip-problem-set.R
+++ R/sip-problem-set.R
@@ -14,14 +14,15 @@ sip_problem <- function(number){
 
   if(missing(number)) stop("Problem number must be specified")
 
-  if(number == 1){
+  prob <- get(paste0("sip_prob_",number))
 
-    prob <- sip(obj.fn = function(x){ sum(x^2) + x[2] },
+  return(prob)
+
+}
+
+sip_prob_1 <- sip(obj.fn = function(x){ sum(x^2) + x[2] },
                 obj.gr = function(x){ 2 * x },
-                x.bounds = list(c(-2,2),c(-5,5)),
-                eq.fn = NULL,
-                eq.gr = NULL,
-                eq.val = NULL,
+                x.bounds = list(c(-2,2),c(-2,2)),
                 ineq.fn = function(x,t){ t * x[1] + (t^2) * x[2] },
                 ineq.bounds = c(-Inf, 0),
                 ineq.tset = c(0,1),
@@ -31,14 +32,10 @@ sip_problem <- function(number){
                 )
 
 
-  } else if(number == 2){
-    # Ex. 3 Sect. 4 JJ Ruckman & A Shapiro (2009)
-    prob <- sip(obj.fn = function(x){ -x[1]^2 + x[2] },
+
+# Ex. 3 Sect. 4 JJ Ruckman & A Shapiro (2009)
+sip_prob_2 <- sip(obj.fn = function(x){ -x[1]^2 + x[2] },
                 obj.gr = function(x){ c(-2 * x[1], 1) },
-                x.bounds = NULL,
-                eq.fn = NULL,
-                eq.gr = NULL,
-                eq.val = NULL,
                 ineq.fn = function(x,t){ 2 * t * x[1] - x[2] - t^2},
                 ineq.bounds = c(-Inf, 0),
                 ineq.tset = c(-1,1),
@@ -47,12 +44,3 @@ sip_problem <- function(number){
                 x.feasible = c(0,0)
     )
 
-  } else {
-
-    stop("SIP problem number ", number, " not found.")
-
-  }
-
-  return(prob)
-
-}
