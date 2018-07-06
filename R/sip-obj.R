fe13415fdbab4f619906c7cfccc7fd535170e196
Joshua Bon <joshuajbon@gmail.com>
2018-07-06 17:26:30 +1000

    Updated to use constraint object.

--- R/sip-obj.R
+++ R/sip-obj.R
@@ -2,131 +2,117 @@
 #'
 #' Makes a \code{sipo} object to be optimised by SIP solvers included in \code{sipr}. \code{sipo} object contains objective function, constraints, and known feasible points (if applicable).
 #'
-#' @param obj.fn Objective function to be minimized.
-#' @param obj.gr (Optional) gradient (vector) of objective function \code{obj.fn}.
-#' @param x.bounds (Optional) Lower and upper bound of \code{fn} input argument \code{x}. As list, or 2-row matrix if multiple.
-#' @param eq.fn (Optional) Equality constraint (vector) function.
-#' @param eq.gr (Optional) gradient (vector) of equality constraint function.
-#' @param eq.val (Optional) Value(s) equality constraint(s) are equal to.
-#' @param ineq.fn Inequality constraint function. First argument for objective input variable \code{x}. Second argument for index set variable \code{t}.
-#' @param ineq.bounds Lower and upper bound of \code{fn} input argument \code{x}. One, but not both, may be infinite.
-#' @param ineq.tset Compact index set for \code{t} in which inequality constraint \code{ineq.fn} must be satisfied. Must be finite.
-#' @param ineq.gr.x (Optional) gradient (vector) of inequality constraint function w.r.t \code{x}.
-#' @param ineq.gr.t (Optional) gradient of inequality constraint function w.r.t \code{t}.
-#' @param x.feasible List (or one) of feasible \code{x}.
-#' @param t.start (Optional) where to start the optimisation of the lower level problem. Defaults to \code{mean(ineq.tset)}.
+#' @param fn Objective function to be minimized.
+#' @param gr (Optional) gradient (vector) of objective function \code{fn}.
+#' @param lower (Optional) Lower bound of \code{x}.
+#' @param upper (Optional) Upper bound of \code{x}.
+#' @param constraint List of constraint objects to be used.
+#' @param x.feasible (Optional) List of feasible x (vector) points for optimisation.
 #' @param ... (Optional) Other items to store in the sipo.
 #' @return A sipo: semi-infinite programming object
 #' @export
 
-sip <- function(obj.fn, obj.gr = NULL, x.bounds = NULL,
-                eq.fn = NULL, eq.gr = NULL, eq.val = NULL,
-                ineq.fn, ineq.bounds, ineq.tset,
-                ineq.gr.x = NULL, ineq.gr.t = NULL,
-                x.feasible, ...){
+sip <- function(fn, gr = NULL, lower = NULL, upper = NULL,
+                constraint, x.feasible = NULL, ...){
 
-  if(missing(obj.fn)) stop("obj.fn required")
-  if(missing(ineq.fn)) stop("ineq.fn required")
-  if(missing(ineq.bounds)) stop("ineq.bounds required")
-  if(missing(ineq.tset)) stop("ineq.tset required")
-  if(missing(x.feasible)) stop("Please provide at least one feasible solution")
+  if(missing(fn)) stop("fn required")
+  if(missing(constraint)) stop("constraint(s) required")
 
-  if(class(ineq.tset) != "numeric" |
-     length(ineq.tset) != 2 |
-     is.infinite(diff(ineq.tset))) stop("ineq.tset must be a numeric with length 2, defining a compact set")
+  if(class(fn) != "function") stop("fn must be function")
 
-  if(class(ineq.bounds) != "numeric" |
-     length(ineq.bounds) != 2 |
-     sum(is.infinite(ineq.bounds)) == 2 ) stop("ineq.bounds must be a numeric with length 2, with at most one infinite bound")
+  if(!missing(gr) & class(gr) != "function") stop("gr must be function if supplied")
+
+  if(length(lower) > 0 & length(upper) > 0 & length(lower) != length(upper)) stop("lower and upper must have same length (or a least one is NULL")
 
   prog <- structure(
     list(
-    obj = list(fn = obj.fn, gr = obj.gr),
-    eq = list(fn = eq.fn, gr = eq.gr, val = eq.val),
-    ineq = list(fn = ineq.fn, bounds = NULL, tset = sort(ineq.tset), gr = list(x = ineq.gr.x, t = ineq.gr.t), type = NULL),
-    x = list(bounds = NULL, feasible = x.feasible),
-    ll = list(fn = NULL, gr = NULL, gen = list(fn = NULL, gr = NULL), tstart = mean(ineq.tset)),
+    obj = list(fn = fn, gr = gr),
+    x = list(lower = lower, upper = upper, feasible = NULL),
+    ineq = list(inf =  list(fn = NULL, gr = NULL, lower = NULL, upper = NULL),
+                fin =  list(fn = NULL, gr = NULL, lower = NULL, upper = NULL),
+                all =  list(fn = NULL, gr = NULL, lower = NULL, upper = NULL)
+                  ),
+    eq = list(fn = NULL, gr = NULL, value = NULL),
+    constraint_list = NULL,
     ...
     ),
   class = "sipo")
 
-  if(!is.null(x.bounds)){
-    # x bounds
-    b_x_mat <- matrix(unlist(x.bounds), nrow = 2)
-    prog$x$bounds <- list(
-      lower = b_x_mat[1,],
-      upper = b_x_mat[2,]
-    )
-  }
-  # inequality constraint bound
-  b_ineq <- sort(ineq.bounds)
-  n_ineq <- sum(is.finite(b_ineq))
-  if(n_ineq == 2){
-    prog$ineq$type <- "both"
-  } else if(is.finite(b_ineq[1])) {
-    prog$ineq$type <- "lower"
-  } else if(is.finite(b_ineq[2])) {
-    prog$ineq$type <- "upper"
+  if("constraint" %in% class(constraint)){
+    prog$constraint_list <- list(constraint)
+  } else{
+    prog$constraint_list <- constraint
   }
 
-  prog$ineq$bounds <-list(lower = rep(b_ineq[1], times = n_ineq), upper = rep(b_ineq[2], times = n_ineq))
+  if(any(!"constraint" %in% sapply(prog$constraint_list,class))) stop("constraint supplied must be of class constraint. See ?inf_ineq_constr.")
 
-  # implement tests for provided functions
+  if(class(x.feasible) != "list"){
+    prog$x$feasible <- list(x.feasible)
+  } else{
+    prog$x$feasible <- x.feasible
+  }
 
-  # define lower level problem(s)
-    prog$ll$gen$fn <- function(par.t, x, bound, ...){
+  # get indices for type of constraint
+  inf_ineq_i <- sapply(prog$constraint_list, function(x) all(c("infinite","inequality") %in% class(x)) )
+  fin_ineq_i <- sapply(prog$constraint_list, function(x) all(c("finite","inequality") %in% class(x)) )
+  eq_i <- sapply(prog$constraint_list, function(x) all(c("finite","equality") %in% class(x)) )
 
-        sc <- ifelse(bound == "lower", 1, -1)
-
-        if(is.null(prog$ineq$gr$t)){
-          gr <- NULL
-        } else {
-          gr <- function(t) prog$ineq$gr$t(x, t)
-        }
-
-        opt <- optim(par = par.t,
-                     fn = function(t) prog$ineq$fn(x=x,t),
-                     gr = gr,
-                     lower = prog$ineq$tset[1], upper = prog$ineq$tset[2],
-                     control = list(fnscale = sc), method = "L-BFGS-B",
-                     ...)
-
-      return(opt)
-
+  # get inf inequality functions, bounds, gradient
+  if(sum(inf_ineq_i) > 0){
+    prog$ineq$inf$fn <- function(x, ...){
+      sapply(prog$constraint_list[inf_ineq_i], function(cn) cn$lowerlevel$fn(x = x, ...))
     }
-
-    if(!is.null(prog$ineq$gr$x)){
-      prog$ll$gen$gr <- function(par.t, x, bound, ...){
-        t_opt <- prog$ll$fn(par.t, x, bound = bound, ...)
-        x_gr <- prog$ineq$gr$x(x, t_opt$par) # envelope / implicit function theorem
+    prog$ineq$inf$lower <- c(lapply(prog$constraint_list[inf_ineq_i], function(cn) cn$lowerlevel$lower), recursive = T)
+    prog$ineq$inf$upper <- c(lapply(prog$constraint_list[inf_ineq_i], function(cn) cn$lowerlevel$upper), recursive = T)
+    if(!any(sapply(prog$constraint_list[inf_ineq_i], function(cn) is.null(cn$lowerlevel$gr)))){
+      prog$ineq$inf$gr <- function(x, ...){
+        sapply(prog$constraint_list[inf_ineq_i], function(cn) cn$lowerlevel$gr(x = x, ...))
       }
     }
+  }
 
-    # set specific problems
-    if(n_ineq == 2){ # two-sided inequality
-      prog$ll$fn <- function(x, ...){
-        c(prog$ll$gen$fn(par.t = prog$ll$tstart, x, bound = "lower", ...),
-          prog$ll$gen$fn(par.t = prog$ll$tstart, x, bound = "upper", ...)
-          )
-      }
-      if(!is.null(prog$ineq$gr$x)){
-        prog$ll$gr <- function(x, ...){
-          c(prog$ll$gen$gr(par.t = prog$ll$tstart, x, bound = "lower", ...)$value,
-            prog$ll$gen$gr(par.t = prog$ll$tstart, x, bound = "upper", ...)$value
-            )
-        }
-      }
-    } else { # one sided inequality
-      prog$ll$fn <- function(x, ...){
-        prog$ll$gen$fn(par.t = prog$ll$tstart, x, bound = prog$ineq$type, ...)$value
-      }
-      if(!is.null(prog$ineq$gr$x)){
-        prog$ll$gr <- function(x, ...){
-          prog$ll$gen$gr(par.t = prog$ll$tstart, x, bound = prog$ineq$type, ...)$value
-        }
+  # get finite inequ  ality functions, bounds, gradient
+  if(sum(fin_ineq_i) > 0){
+    prog$ineq$fin$fn <- function(x, ...){
+      sapply(prog$constraint_list[fin_ineq_i], function(cn) cn$fn(x = x, ...))
+    }
+    prog$ineq$fin$lower <- c(lapply(prog$constraint_list[fin_ineq_i], function(cn) cn$lower), recursive = T)
+    prog$ineq$fin$upper <- c(lapply(prog$constraint_list[fin_ineq_i], function(cn) cn$upper), recursive = T)
+    if(!any(sapply(prog$constraint_list[fin_ineq_i], function(cn) is.null(cn$gr)))){
+      prog$ineq$fin$gr <- function(x, ...){
+        sapply(prog$constraint_list[fin_ineq_i], function(cn) cn$gr(x = x, ...))
       }
     }
+  }
+
+  if(sum(inf_ineq_i) + sum(fin_ineq_i) > 0){
+
+    all_ineq <- c(list(prog$ineq$inf),list(prog$ineq$fin))
+    all_ineq <- all_ineq[sapply(all_ineq, function(cn) !is.null(cn$fn))]
+
+    prog$ineq$all$fn <- function(x, ...){
+      sapply(all_ineq, function(cn) cn$fn(x = x, ...))
+    }
+    prog$ineq$all$lower <- c(lapply(all_ineq, function(cn) cn$lower), recursive = T)
+    prog$ineq$all$upper <- c(lapply(all_ineq, function(cn) cn$upper), recursive = T)
+    if(!any(sapply(all_ineq, function(cn) is.null(cn$gr)))){
+      prog$ineq$all$gr <- function(x, ...){
+        sapply(all_ineq, function(cn) cn$gr(x = x, ...))
+      }
+    }
+  }
+
+  if(sum(eq_i) > 0){
+    prog$eq$fn <- function(x, ...){
+      sapply(prog$constraint_list[eq_i], function(cn) cn$fn(x = x, ...))
+    }
+    prog$eq$value <- c(lapply(prog$constraint_list[eq_i], function(cn) cn$value), recursive = T)
+    if(!any(sapply(prog$constraint_list[eq_i], function(cn) is.null(cn$gr)))){
+      prog$eq$gr <- function(x, ...){
+        sapply(prog$constraint_list[eq_i], function(cn) cn$gr(x = x, ...))
+      }
+    }
+  }
 
   return(prog)
 }
-
