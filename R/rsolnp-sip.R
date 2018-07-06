427fcb8eead129b86fc0a19be083eca944e1feed
Joshua Bon <joshuajbon@gmail.com>
2018-07-06 17:32:53 +1000

    Changes to incorporate constraint object

--- R/rsolnp-sip.R
+++ R/rsolnp-sip.R
@@ -3,7 +3,7 @@
 #' Wrapper for \code{\link[Rsolnp]{solnp}} to solve semi-infinite program. \code{solnp} uses the augmented Lagrange method for non-linear optimisation.
 #'
 #' @param pars starting parameter vector.
-#' @param sipo SIP object created by \code{\link{make_sip}}.
+#' @param sip SIP object created by \code{\link{make_sip}}.
 #' @param control See control argument in \code{\link[Rsolnp]{solnp}}.
 #' @return Results from \code{\link[Rsolnp]{solnp}} as sipo object (see \code{\link{sip}}).
 #' @export
@@ -16,12 +16,12 @@ optim_solnp <- function(pars, sip, control = list(), ...){
     solnp(pars = pars,
           fun = sip$obj$fn,
           eqfun = sip$eq$fn,
-          eqB = sip$eq$val,
-          ineqfun = sip$ll$fn, # lower level problem function (not sip$ineq$fn)
-          ineqLB = sip$ineq$bounds$lower,
-          ineqUB = sip$ineq$bounds$upper,
-          LB = sip$x$bounds$lower,
-          UB = sip$x$bounds$upper,
+          eqB = sip$eq$value,
+          ineqfun = sip$ineq$all$fn,
+          ineqLB = sip$ineq$all$lower,
+          ineqUB = sip$ineq$all$upper,
+          LB = sip$lower,
+          UB = sip$upper,
           control = control, ...)
 
   # convert to standardised output
